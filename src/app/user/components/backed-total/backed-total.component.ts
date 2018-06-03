ff0ae5d79ee43a76820f6a2bfb7886e55b3270d6
Cristopher Araya <ChrArayaS@gmail.com>
2018-06-03 19:05:55 -0400

    descarga de csv

--- src/app/user/components/backed-total/backed-total.component.ts
+++ src/app/user/components/backed-total/backed-total.component.ts
@@ -17,6 +17,7 @@ import { User } from '../../../core/models/user';
 import { Subscription } from 'rxjs/Subscription';
 import { AuthUser } from '../../../core/models/auth-user';
 import { ToastyService } from 'ng2-toasty';
+import { ModalDirective } from 'ngx-bootstrap';
 
 @Component({
   selector: 'app-backed-total',
@@ -29,12 +30,12 @@ export class BackedTotalComponent implements OnInit {
   @Input() user: User;
   @Output()
   changeView = new EventEmitter<number>();
-
+  @ViewChild('lgModal') lgModal: ModalDirective;
   @Output()
   teamId = new EventEmitter<any>();
   @ViewChild('imageUpload') imageUpload: ImageUploadComponent;
   selectedDate = new Date();
-
+  projectId: any;
   formSubmit = false;
   projectForm: FormGroup;
   categories = [];
@@ -42,15 +43,12 @@ export class BackedTotalComponent implements OnInit {
   authUser: AuthUser;
   role_id: any;
 
-  rows = [
-    { name: 'Austin', gender: 'Male', company: 'Swimlane' },
-    { name: 'Dany', gender: 'Male', company: 'KFC' },
-    { name: 'Molly', gender: 'Female', company: 'Burger King' },
-  ];
+  rows = [];
   columns = [
-    { prop: 'name' },
-    { name: 'Gender' },
-    { name: 'Company' }
+    { prop: 'Nombre' },
+    { prop: 'Telefono' },
+    { prop: 'Email' },
+    { prop: 'Monto' }
   ];
 
   constructor(
@@ -87,13 +85,77 @@ export class BackedTotalComponent implements OnInit {
     return this.userService.isLoggedInUser(this.user) && this.user.role_name === 'admin';
   }
 
-  changeTab(id){
-    this.teamHttpService.getTeam(id).subscribe((data) => {
-      this.teamId.emit(data.project);
-      this.changeView.emit(10);
+  openModal(id){
+    this.teamHttpService.getBackers(id).subscribe((data) => {
+      this.rows = data.project;
+      this.projectId = id;
+      this. showModal();
     });
     //this.teamId.emit(id)    
   }
+
+  showModal() {
+    this.lgModal.show();
+  }
+
+  hideModal() {
+    this.lgModal.hide();
+  }
+
+  download(){
+    this.teamHttpService.getBackers(this.projectId).subscribe((data) => {
+      let csvHeader = [
+        'Nombre',
+        'Telefono',
+        'Email',
+        'Monto'
+      ]
+      let csv =  data.project;
+      let csvData = this.ConvertToCSV(csv, csvHeader);
+      let blob = new Blob([csvData], { type: 'text/csv' });
+      let dwldLink = document.createElement("a");
+      let url= window.URL.createObjectURL(blob);
+        let isSafariBrowser = navigator.userAgent.indexOf('Safari') != -1 && navigator.userAgent.indexOf('Chrome') == -1;
+          if (isSafariBrowser) { //if Safari open in new window to save file with random filename.
+              dwldLink.setAttribute("target", "_blank");
+          }
+          dwldLink.setAttribute("href", url);
+          dwldLink.setAttribute("download", "Enterprise.csv");
+          dwldLink.style.visibility = "hidden";
+          document.body.appendChild(dwldLink);
+          dwldLink.click();
+          document.body.removeChild(dwldLink);
+    });
+
+    
+    
+  }
+
+  ConvertToCSV(objArray, headerList) {
+    //console.log('convert to array called');
+     var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+     var str = '';
+     var row = 'S.No,';
+
+     for (var index in headerList) { //objArray[0]
+         //Now convert each value to string and comma-separated
+         row += headerList[index] + ',';
+     }
+     row = row.slice(0, -1);
+     //append Label row with line break
+     str += row + '\r\n';
+     for (var i = 0; i < array.length; i++) {
+         var line = (i+1)+'';
+         for (var index in headerList) {//array[i]
+            let head = headerList[index];
+
+             //if (line != '') line += ','
+             line += ',' + array[i][head];
+         }
+         str += line + '\r\n';
+     }
+     return str;
+ }
 
 
 
